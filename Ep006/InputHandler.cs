8e7daf45db889da1ff941a4bf1e5340867129db0
bigmekben <49128474+bigmekben@users.noreply.github.com>
2025-01-20 18:01:00 -0500

    Ep 006

---
+++ Ep006/InputHandler.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+namespace SG
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        public bool circleInput;
+        public bool rollFlag;
+        public bool sprintFlag;
+        public float rollInputTimer;
+
+        InputSystem_Actions inputActions;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+
+
+        public void OnEnable()
+        {
+            if(inputActions == null)
+            {
+                inputActions = new InputSystem_Actions();
+                inputActions.Player.Move.performed += inputAction => movementInput = inputAction.ReadValue<Vector2>();
+                inputActions.Player.Look.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+            HandleCircleInput(delta);
+        }
+
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
+
+        private void HandleCircleInput(float delta)
+        {
+            circleInput = inputActions.Player.Crouch.phase == UnityEngine.InputSystem.InputActionPhase.Performed;
+            if(circleInput)
+            {
+                rollInputTimer += delta;
+                sprintFlag = true;
+            }
+            else
+            {
+                if (rollInputTimer > 0 && rollInputTimer < 0.5f)
+                {
+                    sprintFlag = false;
+                    rollFlag = true;
+                }
+                rollInputTimer = 0;
+            }
+        }
+
+    }
+}
