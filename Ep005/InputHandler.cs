84808b393b876b9d41bfd52cd10b2ac58b3af7c9
bigmekben <49128474+bigmekben@users.noreply.github.com>
2025-01-20 16:37:52 -0500

    episode 005

---
+++ Ep005/InputHandler.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+namespace SG
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        public bool circleInput;
+        public bool rollFlag;
+        public bool sprintFlag;
+        public float rollInputTimer;
+        public bool isInteracting;
+
+        InputSystem_Actions inputActions;
+        CameraHandler cameraHandler;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        private void Awake()
+        {
+            cameraHandler = CameraHandler.singleton; //GetComponent<CameraHandler>();
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, mouseX, mouseY);
+            }
+        }
+
+        public void OnEnable()
+        {
+            if(inputActions == null)
+            {
+                inputActions = new InputSystem_Actions();
+                inputActions.Player.Move.performed += inputAction => movementInput = inputAction.ReadValue<Vector2>();
+                inputActions.Player.Look.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+            HandleCircleInput(delta);
+        }
+
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
+
+        private void HandleCircleInput(float delta)
+        {
+            circleInput = inputActions.Player.Crouch.phase == UnityEngine.InputSystem.InputActionPhase.Performed;
+            if(circleInput)
+            {
+                rollInputTimer += delta;
+                sprintFlag = true;
+            }
+            else
+            {
+                if (rollInputTimer > 0 && rollInputTimer < 0.5f)
+                {
+                    sprintFlag = false;
+                    rollFlag = true;
+                }
+                rollInputTimer = 0;
+            }
+        }
+
+    }
+}
