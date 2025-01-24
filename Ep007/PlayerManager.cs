4b72c6bee4fbbe8739e5d87d700cc5f3c1e7024f
bigmekben <49128474+bigmekben@users.noreply.github.com>
2025-01-24 06:05:15 -0500

    Episode 7

---
+++ Ep007/PlayerManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SG
+{
+    public class PlayerManager : MonoBehaviour
+    {
+
+        InputHandler inputHandler;
+        Animator anim;
+        CameraHandler cameraHandler;
+        PlayerLocomotion playerLocomotion;
+
+        [Header("Player Flags")]
+        public bool isInteracting;
+        public bool isSprinting;
+        public bool isInAir;
+        public bool isGrounded;
+
+        private void Awake()
+        {
+            cameraHandler = FindFirstObjectByType<CameraHandler>();
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            anim = GetComponentInChildren<Animator>();
+            playerLocomotion = GetComponent<PlayerLocomotion>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float delta = Time.deltaTime;
+
+            isInteracting = anim.GetBool("isInteracting");
+
+            inputHandler.TickInput(delta);
+            playerLocomotion.HandleMovement(delta);
+            playerLocomotion.HandleRollingAndSprinting(delta);
+            playerLocomotion.HandleFalling(delta, playerLocomotion.moveDirection); // couldn't HandleFalling just access moveDirection directly?
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, inputHandler.mouseX, inputHandler.mouseY);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            inputHandler.rollFlag = false;
+            inputHandler.sprintFlag = false;
+            isSprinting = inputHandler.circleInput;
+
+            if (isInAir)
+            {
+                playerLocomotion.inAirTimer += Time.deltaTime;
+            }
+        }
+    }
+}
