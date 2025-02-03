4fe8b708e19d0f5ca8bb85376f31520f636fc298
bigmekben <49128474+bigmekben@users.noreply.github.com>
2025-02-03 08:55:56 -0500

    Ep 17

---
+++ ep017/PlayerManager.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SG
+{
+    public class PlayerManager : MonoBehaviour
+    {
+
+        InputHandler inputHandler;
+        Animator anim;
+        CameraHandler cameraHandler;
+        PlayerLocomotion playerLocomotion;
+
+        [Header("Player Flags")]
+        public bool isInteracting;
+        public bool isSprinting;
+        public bool isInAir;
+        public bool isGrounded;
+        public bool canDoCombo;
+
+        private void Awake()
+        {
+            cameraHandler = FindFirstObjectByType<CameraHandler>();
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            anim = GetComponentInChildren<Animator>();
+            playerLocomotion = GetComponent<PlayerLocomotion>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float delta = Time.deltaTime;
+
+            isInteracting = anim.GetBool("isInteracting");
+            canDoCombo = anim.GetBool("canDoCombo");
+
+            inputHandler.TickInput(delta);
+            playerLocomotion.HandleMovement(delta);
+            playerLocomotion.HandleRollingAndSprinting(delta);
+            playerLocomotion.HandleFalling(delta, playerLocomotion.moveDirection); // couldn't HandleFalling just access moveDirection directly?
+            CheckForInteractableObject();
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, inputHandler.mouseX, inputHandler.mouseY);
+            }
+        }
+
+        private void LateUpdate()
+        {
+            inputHandler.rollFlag = false;
+            inputHandler.sprintFlag = false;
+            inputHandler.rbInput = false;
+            inputHandler.rtInput = false;
+            inputHandler.dPadLeft = false;
+            inputHandler.dPadRight = false;
+            inputHandler.dPadUp = false;
+            inputHandler.dPadDown = false;
+            inputHandler.aInput = false;
+
+            if (isInAir)
+            {
+                playerLocomotion.inAirTimer += Time.deltaTime;
+            }
+        }
+
+        public void CheckForInteractableObject()
+        {
+            RaycastHit hit;
+            if (Physics.SphereCast(transform.position, 0.3f, transform.forward, out hit, 1f, cameraHandler.ignoreLayers))
+            {
+                if (hit.collider.tag == "Interactable")
+                {
+                    Interactable interactableObject = hit.collider.GetComponent<Interactable>();
+                    if (interactableObject != null)
+                    {
+                        string interactableText = interactableObject.interactableText;
+                        // set the UI text to interactable text
+                        // set text pop up to true
+                        if (inputHandler.aInput)
+                        {
+                            hit.collider.GetComponent<Interactable>().Interact(this);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
