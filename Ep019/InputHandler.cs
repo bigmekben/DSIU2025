61a7ed1241d2925fec5ba31899304b06a874a01a
bigmekben <49128474+bigmekben@users.noreply.github.com>
2025-02-07 07:20:31 -0500

    Finished Episode 19

---
+++ Ep019/InputHandler.cs
@@ -0,0 +1,178 @@
+using UnityEngine;
+
+namespace SG
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        public bool bInput;
+        public bool aInput;
+        public bool yInput;
+        public bool xInput;
+        public bool rbInput;
+        public bool rtInput;
+        public bool lbInput;
+        public bool ltInput;
+        public bool dPadUp;
+        public bool dPadDown;
+        public bool dPadLeft;
+        public bool dPadRight;
+
+        public bool rollFlag;
+        public bool sprintFlag;
+        public bool comboFlag;
+        public float rollInputTimer;
+
+        InputSystem_Actions inputActions;
+        PlayerAttacker playerAttacker;
+        PlayerInventory playerInventory;
+        PlayerManager playerManager;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        private void Awake()
+        {
+            playerAttacker = GetComponent<PlayerAttacker>();
+            playerInventory = GetComponent<PlayerInventory>();
+            playerManager = GetComponent<PlayerManager>();
+        }
+
+        public void OnEnable()
+        {
+            if(inputActions == null)
+            {
+                inputActions = new InputSystem_Actions();
+                inputActions.Player.Move.performed += inputAction => movementInput = inputAction.ReadValue<Vector2>();
+                inputActions.Player.Look.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+            HandleCircleInput(delta);
+            HandleAttackInput(delta);
+            HandleQuickSlotsInput();
+            HandleInteractingButtonInput();
+            HandleYTriangleInput();
+        }
+
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
+
+        private void HandleCircleInput(float delta)
+        {
+            bInput = inputActions.Player.CircleButton.phase == UnityEngine.InputSystem.InputActionPhase.Performed;
+            if(bInput)
+            {
+                rollInputTimer += delta;
+                if (moveAmount > 0.5f)
+                {
+                    sprintFlag = true;
+                }
+            }
+            else
+            {
+                if (rollInputTimer > 0 && rollInputTimer < 0.5f)
+                {
+                    sprintFlag = false;
+                    rollFlag = true;
+                }
+                rollInputTimer = 0;
+            }
+        }
+
+        private void HandleAttackInput(float delta)
+        {
+            inputActions.Player.RB.performed += i => rbInput = true;
+            inputActions.Player.RT.performed += i => rtInput = true;
+
+            if(rbInput)
+            {
+                if (playerManager.canDoCombo)
+                {
+                    comboFlag = true;
+                    playerAttacker.HandleWeaponCombo(playerInventory.rightWeapon);
+                    comboFlag = false;
+                }
+                else
+                {
+                    if (playerManager.isInteracting)
+                    {
+                        return;
+                    }
+                    if (playerManager.canDoCombo)
+                    {
+                        return;
+                    }
+                    playerAttacker.HandleLightAttack(playerInventory.rightWeapon);
+                }
+            }
+
+            if (rtInput)
+            {
+                if (playerManager.isInteracting)
+                {
+                    return;
+                }
+                if (playerManager.canDoCombo)
+                {
+                    return;
+                }
+                if (playerManager.canDoCombo)
+                {
+
+                }
+
+                playerAttacker.HandleHeavyAttack(playerInventory.rightWeapon);
+            }
+        }
+
+        private void HandleQuickSlotsInput()
+        {
+            inputActions.Player.Previous.performed += i => dPadLeft = true;
+            inputActions.Player.Next.performed += i => dPadRight = true;
+            if (dPadRight)
+            {
+                playerAttacker.EquipRightWaist();
+                // to do: attach a "holster" object to the player's waist.  EquipRightWaist should tell it 
+                // which model to display depending on the recently-unequipped weaponItem.  So should pass some params to EquipRightWaist after all.
+                // should be fine for now though.
+                playerInventory.ChangeRightWeapon();
+            }
+            if (dPadLeft)
+            {
+                // playerInventory.ChangeLeftWeapon();
+            }
+        }
+
+        private void HandleInteractingButtonInput()
+        {
+            inputActions.Player.XButton.performed += i => aInput = true;
+
+        }
+
+        private void HandleYTriangleInput()
+        {
+            inputActions.Player.TriangleButton.performed += i => yInput = true;
+        }
+    }
+}
---
+++ Ep019/PlayerLocomotion.cs
@@ -0,0 +1,326 @@
+using UnityEngine;
+
+// As of episode 7, this has a few tweaks that Ben made over what was shown in the tutorial video.
+// As of this check-in, the behavior isn't perfect; the player gets stuck on edge, doesn't walk up stairs properly, etc.
+// Will try to tweak it later.  Not sure if there's something wrong with the animation, size of capsule collider, parameters to this script, etc.
+
+namespace SG
+{
+    public class PlayerLocomotion : MonoBehaviour
+    {
+        PlayerManager playerManager;
+        Transform cameraObject;
+        InputHandler inputHandler;
+        PlayerStats playerStats;
+        public Vector3 moveDirection;
+
+        [HideInInspector]
+        public Transform myTransform;
+        [HideInInspector]
+        public AnimatorHandler animatorHandler;
+
+        // to do: calculate these depending on stats and equipment load
+        public int staminaNeededForRoll = 20;
+        public int staminaNeededForBackstep = 20;
+        public int staminaNeededForSprint = 1;
+
+        public new Rigidbody rigidbody; // tbd: is "new" needed?
+        public GameObject normalCamera;
+
+        [Header("Ground & Air Detection Stats")]
+        [SerializeField]
+        [Tooltip("body's distance from center to toe")]
+        float toeDepth = 0.4f; // tutorial uses 0.4.
+        [SerializeField]
+        [Tooltip("minimum height of obstacle you can't simply walk up - typically just over the body's ankle bone")]
+        float tooHighStepHeight = 0.4f; // tutorial calls this "groundDetectionRayStartPoint"
+        // The stairs in my house are 7 5/8", or about an inch above my ankle bone;
+        // so I got 0.4 on my model by positioning the bottom of the capsule collider .
+        [SerializeField]
+        [Tooltip("if body was falling for at least this long, it will switch to a landing anim when touching ground")]
+        float minimumFallTime = 0.5f;
+        [SerializeField]
+        [Tooltip("while falling, movement speed will be divided by this number")]
+        float fallDampening = 2f;
+
+        [SerializeField]
+        float minimumDistanceNeededToBeginFall = 1f; 
+        // player is 1.79 units tall; so this is falling from about hip height.
+        // knee height would be 0.54.  Would be nice to add a secondary land animation for falling from knee-to-hip height.
+
+        [SerializeField]
+        float groundDirectionRayDistance = 0.2f;
+
+        LayerMask ignoreForGroundCheck;
+        public float inAirTimer;
+
+        [Header("Movement Stats")]
+        [SerializeField]
+        float movementSpeed = 4f;
+        [SerializeField]
+        float sprintSpeed = 7f;
+        [SerializeField]
+        float rotationSpeed = 10f;
+        [SerializeField]
+        float fallingSpeed = 80f;
+        [SerializeField]
+        float jumpForce = 10f;
+
+        private void Start()
+        {
+            playerManager = GetComponent<PlayerManager>();
+            rigidbody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            animatorHandler = GetComponentInChildren<AnimatorHandler>();
+            playerStats = GetComponent<PlayerStats>();
+            cameraObject = Camera.main.transform;
+            myTransform = transform;
+            animatorHandler.Initialize();
+            playerManager.isGrounded = true;
+            ignoreForGroundCheck = ~(1 << 8 | 1 << 11);
+        }
+
+
+        #region Movement
+
+        Vector3 normalVector;
+        Vector3 targetPosition;
+
+        private void HandleRotation(float delta)
+        {
+            Vector3 targetDir = Vector3.zero;
+            float moveOverride = inputHandler.moveAmount;
+
+            targetDir = cameraObject.forward * inputHandler.vertical;
+            targetDir += cameraObject.right * inputHandler.horizontal;
+
+            targetDir.Normalize();
+            targetDir.y = 0;
+
+            if (targetDir == Vector3.zero)
+            {
+                targetDir = myTransform.forward;
+            }
+
+            float rs = rotationSpeed;
+
+            Quaternion tr = Quaternion.LookRotation(targetDir);
+            Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+            myTransform.rotation = targetRotation;
+        }
+
+        public void HandleMovement(float delta)
+        {
+            if(inputHandler.rollFlag)
+            {
+                return;
+            }
+
+            if(playerManager.isInteracting)
+            {
+                animatorHandler.SetFloat("Vertical", 0); // BJT fix Jan 27, 2025
+                animatorHandler.SetFloat("Horizontal", 0); // BJT fix Jan 27, 2025
+                return;
+            }
+
+            moveDirection = cameraObject.forward * inputHandler.vertical;
+            moveDirection += cameraObject.right * inputHandler.horizontal;
+            moveDirection.Normalize();
+            moveDirection.y = 0;
+
+            float speed = movementSpeed * inputHandler.moveAmount;
+
+            if (inputHandler.sprintFlag && playerStats.currentStamina >= staminaNeededForSprint)
+            {
+                speed = sprintSpeed;
+                playerManager.isSprinting = true;
+                playerStats.TakeStaminaDamage(staminaNeededForSprint);
+            }
+            else
+            {
+                playerManager.isSprinting = false;
+            }
+            moveDirection *= speed;
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+            rigidbody.linearVelocity = projectedVelocity;
+
+            animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0, playerManager.isSprinting);
+
+            if (animatorHandler.canRotate)
+            {
+                HandleRotation(delta);
+            }
+        }
+
+        public void HandleRollingAndSprinting(float delta)
+        {
+            if (animatorHandler.anim.GetBool("isInteracting"))
+            {
+                return;
+            }
+
+            if (inputHandler.rollFlag)
+            {
+                moveDirection = cameraObject.forward * inputHandler.vertical;
+                moveDirection += cameraObject.right * inputHandler.horizontal;
+
+                if(inputHandler.moveAmount > 0)
+                {
+                    if(playerStats.currentStamina >= staminaNeededForRoll)
+                    {
+                        animatorHandler.PlayTargetAnimation("Rolling", true);
+                        moveDirection.y = 0;
+                        Quaternion rollRotation = Quaternion.LookRotation(moveDirection);
+                        myTransform.rotation = rollRotation;
+                    }
+                }
+                else
+                {
+                    if(playerStats.currentStamina >= staminaNeededForBackstep)
+                    {
+                        animatorHandler.PlayTargetAnimation("Backstep", true);
+                    }
+                }
+            }
+        }
+
+        public void HandleFalling(float delta, Vector3 moveDirection)
+        {
+            playerManager.isGrounded = false;
+            RaycastHit hit;
+            Vector3 origin = myTransform.position;
+            origin.y += tooHighStepHeight;
+
+            Debug.DrawRay(origin, myTransform.forward * toeDepth, Color.red);
+            // Prevent lateral movement if the step height or slope is too high or would look awkward
+            // (or if you are falling while pressing up to a wall or cliff):
+            if(Physics.Raycast(origin, myTransform.forward, out hit, toeDepth))
+            {
+                moveDirection = Vector3.zero;
+            }
+
+            // Simulate gravity's work, with a 20% forward momentum.
+            if (playerManager.isInAir)
+            {
+                rigidbody.AddForce(-Vector3.up * fallingSpeed);
+                rigidbody.AddForce(moveDirection * fallingSpeed / 5f);
+            }
+
+            Vector3 dir = moveDirection;
+            dir.Normalize();
+            origin += dir * groundDirectionRayDistance;
+
+            targetPosition = myTransform.position;
+
+            Debug.DrawRay(origin, -Vector3.up * minimumDistanceNeededToBeginFall, Color.red);
+            // stop vertical movement when feet touch ground/platform:
+            if (Physics.Raycast(origin, -Vector3.up, out hit, minimumDistanceNeededToBeginFall, ignoreForGroundCheck))
+            {
+                normalVector = hit.normal;
+                Vector3 tp = hit.point;
+                playerManager.isGrounded = true;
+                targetPosition.y = tp.y;
+
+                // if body was falling for long enough, show landing animation;
+                // otherwise let the animation stay the same.
+                if(playerManager.isInAir)
+                {
+                    if(inAirTimer > minimumFallTime)
+                    {
+                        Debug.Log($"You were in the air for {inAirTimer}");
+                        animatorHandler.PlayTargetAnimation("Land", true);
+                        inAirTimer = 0;
+                    }
+                    else
+                    {
+                        animatorHandler.PlayTargetAnimation("Empty", false);
+                        inAirTimer = 0;
+                    }
+
+                    playerManager.isInAir = false;
+                }
+            }
+            else
+            {
+                // Nothing under the body's feet in lateral movement direction:
+                if (playerManager.isGrounded)
+                {
+                    playerManager.isGrounded = false;
+                }
+
+                // Only start the anim and set the new velocity once, when fall begins:
+                if(playerManager.isInAir == false)
+                {
+                    if(playerManager.isInteracting == false)
+                    {
+                        animatorHandler.PlayTargetAnimation("Falling", true);
+                    }
+
+                    Vector3 vel = rigidbody.linearVelocity;
+                    vel.Normalize();
+                    rigidbody.linearVelocity = vel * (movementSpeed / fallDampening);
+                    playerManager.isInAir = true; // serves as a debounce.
+                }
+            }
+
+            // From Episode 11:
+            if (playerManager.isInteracting || inputHandler.moveAmount > 0)
+            {
+                myTransform.position = Vector3.Lerp(myTransform.position, targetPosition, Time.deltaTime);
+            }
+            else
+            {
+                myTransform.position = targetPosition;
+            }
+
+            //// From the original "falling" video:
+            //// body may have just "landed" (touched ground) during this frame, or where already on the ground from a
+            //// previous frame:
+            //if (playerManager.isGrounded)
+            //{
+            //    if(playerManager.isInteracting || inputHandler.moveAmount > 0)
+            //    {
+            //        myTransform.position = Vector3.Lerp(myTransform.position, targetPosition, Time.deltaTime);
+            //        // Not sure this block does much; the only difference would be the y of targetPosition.
+            //        // I suppose the point is to ease into the collision point (only the y coordinate would be impacted).
+            //    }
+            //    else
+            //    {
+            //        myTransform.position = targetPosition;
+            //    }
+            //}
+        }
+
+        public void HandleJumping()
+        {
+            if(playerManager.isInteracting)
+            {
+                return;
+            }
+
+            if(inputHandler.yInput)
+            {
+                if(inputHandler.moveAmount > 0)
+                {
+                    moveDirection = cameraObject.forward * inputHandler.vertical;
+                    moveDirection += cameraObject.right * inputHandler.horizontal;
+                    // from roblatham4122 on Episode 19:
+
+                    //Vector3 currentVelocity = GetComponent<Rigidbody>().linearVelocity;
+                    //Vector3 jumpDirection = (moveDirection + Vector3.up).normalized;
+                    //GetComponent<Rigidbody>().linearVelocity = currentVelocity + jumpDirection * jumpForce;
+
+                    // BT: also match the collider's position
+
+                    animatorHandler.PlayTargetAnimation("Jump", false); // true rec. by SG, false rec. by @DiegoGrajales12 on Ep 19
+                    moveDirection.y = 0; // root motion will take care of y movement
+                    Quaternion jumpRotation = Quaternion.LookRotation(moveDirection);
+                    myTransform.rotation = jumpRotation;
+                }
+            }
+        }
+        #endregion
+    }
+}
